683b2039f29bab169ebbd81e6dbc99e45160f30a
Brey <breixosanchez@gmail.com>
2022-11-14 10:58:47 -0800

    Update Amazon interview question.sql

--- SQL queries/Amazon interview question.sql
+++ SQL queries/Amazon interview question.sql
@@ -11,3 +11,16 @@ SELECT
 year_month,
 AVG(total_amt) OVER(ORDER BY year_month ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
 FROM montly_amt
+
+-- This is the final result that we obtain:
+-- year_month	avg
+-- 2020-01	26292
+-- 2020-02	23493.5
+-- 2020-03	25535.667
+-- 2020-04	24082.667
+-- 2020-05	25417.667
+-- 2020-06	24773.333
+-- 2020-07	25898.667
+-- 2020-08	25497.333
+-- 2020-09	24544
+-- 2020-10	21211
