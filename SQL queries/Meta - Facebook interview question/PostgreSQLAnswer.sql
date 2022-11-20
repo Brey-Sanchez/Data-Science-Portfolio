97e89f71874b2001f9dd330b34a9e6946a92ddf2
Brey <breixosanchez@gmail.com>
2022-11-20 12:35:40 -0600

    Create PostgreSQLAnswer.sql

---
+++ SQL queries/Meta - Facebook interview question/PostgreSQLAnswer.sql
@@ -0,0 +1,19 @@
+WITH all_users AS (
+  (SELECT
+  user1 AS user_id,
+  COUNT(user2) AS n_friends
+  FROM facebook_friends
+  GROUP BY 1)
+  UNION
+  (SELECT
+  user2 AS user_id,
+  COUNT(user1) AS n_friends
+  FROM facebook_friends
+  GROUP BY 1)
+ )
+SELECT
+user_id,
+SUM(n_friends) / (SELECT COUNT(DISTINCT(user_id)) FROM all_users) * 100 AS popularity_perc
+FROM all_users
+GROUP BY 1
+ORDER BY 1;
