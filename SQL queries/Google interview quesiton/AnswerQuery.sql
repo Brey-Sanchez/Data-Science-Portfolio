4d1be542bbfe4f36488f1495ab59aa9558e7c5e0
Brey <breixosanchez@gmail.com>
2022-11-28 13:00:31 -0600

    Create AnswerQuery.sql

---
+++ SQL queries/Google interview quesiton/AnswerQuery.sql
@@ -0,0 +1,19 @@
+-- Creating a CTE with an array for the words and an array for the the number of occurrences
+-- of each word in each file
+WITH occurrences AS(
+    SELECT 
+    UNNEST(ARRAY['bull', 'bear']) AS word,
+    UNNEST(ARRAY[
+                 (LENGTH(contents) - LENGTH(REPLACE(contents, ' bull ', ''))) / LENGTH(' bull '),
+                 (LENGTH(contents) - LENGTH(REPLACE(contents, ' bear ', ''))) / LENGTH(' bear ')
+                 ]
+                 ) AS nentry
+    FROM google_file_store
+)
+-- Obtaining the requested table
+SELECT
+word,
+SUM(nentry) AS nentry
+FROM occurrences
+GROUP BY 1
+ORDER BY 1 DESC;
